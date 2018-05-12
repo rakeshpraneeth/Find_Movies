5760202cb9c10ab68f4ae03ea0a1f6f2173c0653
Rakesh Konduru <rakeshpraneeth@gmail.com>
2018-05-12 16:35:54 -0400

    * Basic setup for retrofit calls.

---
+++ app/src/main/java/com/krp/findmovies/interfaces/FmApiService.java
@@ -0,0 +1,11 @@
+package com.krp.findmovies.interfaces;
+
+/**
+ * Created by rakeshpraneeth.
+ * This interface is used to handle all the API calls using Retrofit.
+ * It contains methods such as GET,POST,...
+ * Each operation will have a method and some parameters depending on the type of operation we perform.
+ */
+
+public interface FmApiService {
+}
